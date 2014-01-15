9a1e317c9aaa7ef3126d02edbf6e66c5cbbeb90f
hirokif <hirokif@gmail.com>
2014-01-15 23:10:25 +0900

    Corpモデル作成

---
+++ db/migrate/20140114081350_create_corps.rb
@@ -0,0 +1,33 @@
+class CreateCorps < ActiveRecord::Migration
+  def change
+    create_table :corps do |t|
+      t.integer :corp_no
+      t.string :corp_name
+      t.string :corp_namek
+      t.string :corp_posi
+      t.string :corp_rep
+      t.string :corp_repk
+      t.date :rep_bth
+      t.string :corp_add
+      t.string :corp_tel
+      t.string :corp_fax
+      t.string :corp_mail
+      t.integer :cap
+      t.integer :cy
+      t.integer :emp
+      t.date :acc
+      t.string :corp2_name
+      t.string :corp2_namek
+      t.string :corp2_posi
+      t.string :corp2_rep
+      t.string :corp2_repk
+      t.date :rep2_bth
+      t.string :corp2_add
+      t.string :corp2_tel
+      t.string :corp2_fax
+      t.string :corp2_mail
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140105043318) do
+ActiveRecord::Schema.define(version: 20140114081350) do
+
+  create_table "corps", force: true do |t|
+    t.integer  "corp_no"
+    t.string   "corp_name"
+    t.string   "corp_namek"
+    t.string   "corp_posi"
+    t.string   "corp_rep"
+    t.string   "corp_repk"
+    t.date     "rep_bth"
+    t.string   "corp_add"
+    t.string   "corp_tel"
+    t.string   "corp_fax"
+    t.string   "corp_mail"
+    t.integer  "cap"
+    t.integer  "cy"
+    t.integer  "emp"
+    t.date     "acc"
+    t.string   "corp2_name"
+    t.string   "corp2_namek"
+    t.string   "corp2_posi"
+    t.string   "corp2_rep"
+    t.string   "corp2_repk"
+    t.date     "rep2_bth"
+    t.string   "corp2_add"
+    t.string   "corp2_tel"
+    t.string   "corp2_fax"
+    t.string   "corp2_mail"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "grps", force: true do |t|
     t.string   "grp_name"
