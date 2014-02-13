56acbe4965ed333cf1cd9503b850ba1a5e110d56
hirokif <hirokif@gmail.com>
2014-02-13 13:50:58 +0900

    rootをcorps#indexに設定

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Cb14::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'corps#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
