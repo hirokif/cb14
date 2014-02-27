b2d0f3c4a208b62052bd4873a455022c7cf6b0cd
hirokif <hirokif@gmail.com>
2014-02-28 00:27:56 +0900

    cdks#index select_tag

--- app/controllers/cdks_controller.rb
+++ app/controllers/cdks_controller.rb
@@ -2,15 +2,18 @@ class CdksController < ApplicationController
   def index
     @corp = Corp.find_by_corp_no(params[:corp_id])
     @cdks = Cdk.find_all_by_corp_no(@corp.corp_no)
-
+    @grps = Grp.all
+    @tois = Toi.all
   end
 
   def create
-
+    @corp = Corp.find(params[:corp_id])
+    @cdk = @corp.cdks.create(cdk_params)
+    redirect_to corp_path(@corp.id)
   end
 
   def edit
-
+    @cdk = Cdk.find(params[:id])
   end
 
   def update
@@ -23,7 +26,7 @@ class CdksController < ApplicationController
 
   private
     def cdk_params
-
+      params.require(:cdk).permit(:pp, :mr, :egr_fr, :egr_bb, :egr_sr, :egr_or)
     end
 
 end
