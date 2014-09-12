d9d3acb9afd61750d38115ffe820a019280b9bcd
hirokif <hirokif@gmail.com>
2014-09-12 17:33:55 +0900

    cdc#new

--- app/controllers/cdcs_controller.rb
+++ app/controllers/cdcs_controller.rb
@@ -1,2 +1,8 @@
 class CdcsController < ApplicationController
+  def new
+    @cdc = Cdc.new
+    @corp = Corp.find(params[:corp_id])
+    @tois = Toi.find_all_by_grp_no(params[:grp_no])
+    @grp = Grp.find(params[:grp_no])
+  end
 end
